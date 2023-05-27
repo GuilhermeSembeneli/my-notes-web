42f642251fd3f9fde117bd0f80b827574bf74e5b
Guilherme Sembeneli <usuario@MacBook-Pro-de-USER.local>
2023-05-26 22:06:13 -0300

    fix: add alert input

--- script.js
+++ script.js
@@ -2,39 +2,40 @@ let notes = [];
 let noteList = null;
 
 window.onload = () => {
-    noteList = document.getElementById('note-list');
-    onLoad();
-}
+  noteList = document.getElementById("note-list");
+  onLoad();
+};
 
 insertNote = () => {
-    if(input.value) {
-        notes.push(input.value);
-        noteList.innerText = '';
-        notes.forEach((item) => {
-            newItem = document.createElement('li');
-            noteList.appendChild(newItem);
-            newItem.innerText = item
-        })
+  if (input.value) {
+    notes.push(input.value);
+    noteList.innerText = "";
+    notes.forEach((item) => {
+      newItem = document.createElement("li");
+      noteList.appendChild(newItem);
+      newItem.innerText = item;
+    });
 
-        input.value = '';
-    } else {
-        notes.push('---');
-        noteList.innerText = '';
-        notes.forEach((item) => {
-            newItem = document.createElement('li');
-            noteList.appendChild(newItem);
-            newItem.innerText = item;
-        })
+    input.value = "";
+  } else {
+    alert("Não digitou nada");
+    notes.push("---");
+    noteList.innerText = "";
+    notes.forEach((item) => {
+      newItem = document.createElement("li");
+      noteList.appendChild(newItem);
+      newItem.innerText = item;
+    });
 
-        input.value = '';
-    }
-}
+    input.value = "";
+  }
+};
 
 onLoad = () => {
-    if (!notes.length) {
-        newItem = document.createElement('li');
-        noteList.appendChild(newItem);
-        newItem.classList.add('disabled-item');
-        newItem.innerText = 'Nada por aqui ainda...';
-    }
-}
+  if (!notes.length) {
+    newItem = document.createElement("li");
+    noteList.appendChild(newItem);
+    newItem.classList.add("disabled-item");
+    newItem.innerText = "Nada por aqui ainda...";
+  }
+};
